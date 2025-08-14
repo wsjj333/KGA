38ec9d9f26b51eb0d61e7850b9a59d6bcaefd7ca
wsjj333 <wsjj333@naver.com>
2025-08-14 09:40:57 +0900

    슈팅게임 생성

---
+++ ShootingGame250814/Intermediate/Build/Win64/x64/ShootingGame250814Editor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32548
+#include "C:/Users/Jeon/Documents/KGA/ShootingGame250814/Intermediate/Build/Win64/x64/ShootingGame250814Editor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
---
+++ ShootingGame250814/Source/ShootingGame250814/ShootingGame250814.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShootingGame250814.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShootingGame250814, "ShootingGame250814" );
